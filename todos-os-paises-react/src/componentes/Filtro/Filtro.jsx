6179a13ec2faceeefc7b23a4bf9267c9ef35cb65
LARA EMANUELY LEITE SOARES <20191D12GR0420@LABS.GARANHUNS.IFPE>
2022-08-16 16:22:10 -0300

    m

---
+++ todos-os-paises-react/src/componentes/Filtro/Filtro.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Filtro () {
+  return (
+    <div className="row">
+        
+        <div className="col-6 col-lg-10">
+        <inpur inputMode="text"/>
+        </div>
+        
+        
+        <div input className="col-3 col-lg-10">
+        <strong>Países: 99</strong>
+        </div>
+
+      
+        <div input class="col-3 col-lg-10">
+        <inpur inputMode="text"/>
+        </div>
+
+
+    </div>
+  )
+}
+
+
+export default Filtro
---
+++ todos-os-paises-react/src/componentes/Titulo/Titulo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App () {
+  return (
+    <h1>Países do mundo</h1>
+  )
+}
+
+export default App 
